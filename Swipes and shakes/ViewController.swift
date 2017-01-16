f47bc3780460bcdc38b696c7c134f92b7dc5d0cd
shailendrasuriyal <shailendra.suriyal@intimetec.com>
2017-01-16 12:23:06 +0530

    Swipe Gesture recogniser and Shake Event Added

--- Swipes and shakes/ViewController.swift
+++ Swipes and shakes/ViewController.swift
@@ -13,13 +13,54 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)));
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)));
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)));
+        swipeUp.direction = UISwipeGestureRecognizerDirection.up
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(gesture:)));
+        swipeDown.direction = UISwipeGestureRecognizerDirection.down
+        
+        self.view.addGestureRecognizer(swipeRight)
+        self.view.addGestureRecognizer(swipeLeft)
+        self.view.addGestureRecognizer(swipeUp)
+        self.view.addGestureRecognizer(swipeDown)
     }
 
+    func swiped(gesture : UISwipeGestureRecognizer) {
+        
+        switch gesture.direction {
+        case UISwipeGestureRecognizerDirection.up:
+            
+            print("Swipe up")
+        case UISwipeGestureRecognizerDirection.left:
+            
+            print("Swipe left")
+        case UISwipeGestureRecognizerDirection.right:
+            
+            print("Swipe right")
+        case UISwipeGestureRecognizerDirection.down:
+            
+            print("Swipe down")
+            
+        default:
+            
+            print("Swiped")
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.motionShake {
+            print("Device as Shaken. . .")
+        }
+    }
+    
 }
 
