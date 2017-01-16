1099dc6cc0d920ba3c538b75c90bf29d12f3154f
shailendrasuriyal <shailendra.suriyal@intimetec.com>
2017-01-16 11:58:08 +0530

    Initial Commit

---
+++ Swipes and shakes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swipes and shakes
+//
+//  Created by Shailendra Suriyal on 16/01/17.
+//  Copyright © 2017 RealDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
